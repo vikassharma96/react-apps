2c0a44cd334651a4aa69d6393c032c53f66bab41
gauravprakash <ergauravprakash@gmail.com>
2021-12-05 14:57:26 +0530

    minor changes

--- src/cart-app/src/App.js
+++ src/cart-app/src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
-      { id: 4, value: 0 }
+      { id: 4, value: 0 },
+      { id: 5, value: 0 }
     ]
   };
 
